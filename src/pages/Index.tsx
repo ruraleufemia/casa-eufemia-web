15e7db8b396560c3c0d21511c0a06d2ecc59f729
ruraleufemia <ruraleufemia@gmail.com>
2025-11-16 12:46:44 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import Footer from "@/components/Footer";
 import SEO from "@/components/SEO";
 
 const Index = () => {
-  const videoUrl = "https://www.youtube.com/embed/kcpwKMlJ9yA?autoplay=1&mute=1&loop=1&playlist=kcpwKMlJ9yA&controls=0&modestbranding=1&rel=0&showinfo=0&vq=hd1080&quality=hd1080";
+  const videoUrl = "https://www.youtube.com/embed/iUC5-n8IP-Q?autoplay=1&mute=1&loop=1&playlist=iUC5-n8IP-Q&controls=0&modestbranding=1&rel=0&showinfo=0&vq=hd1080&quality=hd1080";
   
   return (
     <>
